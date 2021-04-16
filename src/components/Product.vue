03ef124161e4f3ee9d4b61a814173345c4a92c48
rafaelflorespereira <rafael_530@hotmail.com>
2021-04-16 08:15:09 -0300

    added filter to product

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -3,8 +3,10 @@
     <img :src="product.imageUrl" class="product__image" alt="product-img" />
     <div class="product__info">
       <h3 class="product__info-name">{{ product.name }}</h3>
-      <p class="product__info-price">{{ product.price }}</p>
-      <p class="product__info-selling-price">{{ product.sellingPrice }}</p>
+      <p class="product__info-price">R&#36;{{ product.price | decimalPrice }}</p>
+      <p class="product__info-selling-price">
+        R&#36;{{ product.sellingPrice | decimalPrice }}
+      </p>
     </div>
   </div>
 </template>
@@ -12,6 +14,11 @@
 <script>
 export default {
   props: ["product"],
+  filters: {
+    decimalPrice(price) {
+      return (price / 100).toFixed(2);
+    },
+  },
 };
 </script>
 
@@ -20,18 +27,30 @@ export default {
   display: flex;
   flex-direction: row;
   height: 10rem;
-  margin: 0 auto;
+  margin: 2rem auto;
   &__image {
     width: 10rem;
-    border: 1px solid #ccc;
+    border: 2px solid #ccc;
     object-fit: cover;
   }
   &__info {
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
     &-name {
+      font-size: 1.5rem;
+      font-weight: 800;
     }
     &-price {
+      font-weight: bold;
+      color: #999;
+      margin: .4rem 0;
     }
     &-selling-price {
+      font-weight: 600;
+      color: black;
+      font-size: 1.5rem;
     }
   }
 }
