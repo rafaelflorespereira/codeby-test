cd0ff5324ade5ce50aac4f2b3171e021520b32bb
rafaelflorespereira <rafael_530@hotmail.com>
2021-04-16 08:38:36 -0300

    Applied font poppins

--- src/App.vue
+++ src/App.vue
@@ -56,14 +56,14 @@ export default {
 html {
   box-sizing: border-box;
 }
+@font-face {
+  font-family: 'Poppins';
+  src: url('assets/fonts/Poppins-Regular.ttf') format("truetype");
+}
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Poppins;
 }
 
 .pop-up {
